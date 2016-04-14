e3916e4319965b144192487e379c27221c8e8911
zhangxicheng <zhangxicheng@36kr.com>
2016-04-14 13:52:06 +0800

    update

--- dataStructor/arrays.go
+++ dataStructor/arrays.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-    
+    //声明并初始化数组
+    // var arr = [10]int {4,6,2,3,0,12,23,45,13,9}
+    arr := [10]int {4,6,2,3,0,12,23,45,13,9}
+    fmt.Println(arr)
+
+    //声明并初始化切片，如果声明了而不用会报错
+    // slice :=[]int{4,6,2};
+
 }
---
+++ sort/bubbleSort.go
@@ -0,0 +1,30 @@
+/**
+ * 冒泡排序
+ * 1.初始化数组
+ * 2.两两比较
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+    //声明并初始化数组
+    arr := [10]int {4,6,2,3,0,12,23,45,13,9}
+    fmt.Println(arr)
+
+    l:=len(arr)
+    for i:=0;i<l;i++ {
+        flag:=true
+        for k:=0;k<l-i-1;k++{
+            if(arr[k]>arr[k+1]){
+                flag = false
+                arr[k],arr[k+1]=arr[k+1],arr[k]
+            }
+        }
+        if flag {
+            break
+        }
+        fmt.Println(arr)
+    }
+}
